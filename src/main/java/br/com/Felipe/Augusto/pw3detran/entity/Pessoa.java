3a3041a7f2cb896ccefa6e3ff7419f1a62d3a275
Felipe <faugusto@etec.com>
2022-04-06 13:57:27 -0300

    Criacao da entidade Pessoa

---
+++ src/main/java/br/com/Felipe/Augusto/pw3detran/entity/Pessoa.java
@@ -0,0 +1,30 @@
+package br.com.Felipe.Augusto.pw3detran.entity;
+
+import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Pessoa
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "idPessoa")
+	public Long id;
+	
+	@Column (name = "nome")
+	public String nome;
+	
+	@Column (name = "endereco")
+	public String endereco;
+	
+}
